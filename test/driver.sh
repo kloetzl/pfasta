7ca2c03d1eea36099513bc6542527b841b0b7a50
Fabian Klötzl <fabian@kloetzl.info>
2015-08-15 16:27:56 +0200

    adds test for invalid file beginning

--- test/driver.sh
+++ test/driver.sh
@@ -4,6 +4,9 @@
 ./genFasta | ./validate
 ./validate test/pass*
 
+# head prints header lines, breaking the fasta structure
+! ( head --verbose test/pass* | ./validate )
+
 
 for filename in test/fail*
 do 
