253872f1e6ffe3ff2db23a203cd9aaa5e850a319
Fabian Klötzl <fabian@kloetzl.info>
2017-10-07 19:43:02 +0200

    improve concat

--- tools/concat.c
+++ tools/concat.c
@@ -1,98 +1,116 @@
 #include <err.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include "common.h"
 #include "pfasta.h"
 
 static size_t line_length = 70;
 
-size_t strncpy_n(char *dest, const char *src, size_t n) {
-	size_t i = 0;
-	for (; i < n && *src; ++i) {
-		*dest++ = *src++;
+void usage(int exit_code);
+void process(const char *file_name);
+
+int main(int argc, char *argv[]) {
+
+	int c;
+	while ((c = getopt(argc, argv, "hL:")) != -1) {
+		switch (c) {
+		case 'h':
+			usage(EXIT_SUCCESS);
+		case 'L': {
+			const char *errstr;
+
+			line_length = strtonum(optarg, 0, INT_MAX, &errstr);
+			if (errstr) errx(1, "line length is %s: %s", errstr, optarg);
+
+			if (!line_length) line_length = INT_MAX;
+			break;
+		}
+		default:
+			usage(EXIT_FAILURE);
+		}
 	}
-	return i;
+
+	argc -= optind, argv += optind;
+	if (argc == 0) {
+		if (!isatty(STDIN_FILENO)) {
+			process("-");
+		} else {
+			usage(EXIT_FAILURE);
+		}
+	}
+
+	for (int i = 0; i < argc; i++) {
+		process(argv[i]);
+	}
+
+	return EXIT_SUCCESS;
 }
 
-void print_seq(const pfasta_seq *ps) {
-	char line[line_length + 1];
-	const char *seq = ps->seq;
+void process(const char *file_name) {
+	int file_descriptor =
+	    strcmp(file_name, "-") == 0 ? STDIN_FILENO : open(file_name, O_RDONLY);
+	if (file_descriptor < 0) err(1, "%s", file_name);
 
-	for (size_t j; *seq; seq += j) {
-		j = strncpy_n(line, seq, line_length);
-		line[j] = '\0';
-		puts(line);
+	pfasta_file pf;
+	int l = pfasta_parse(&pf, file_descriptor);
+	if (l != 0) {
+		errx(1, "%s: %s", file_name, pfasta_strerror(&pf));
 	}
+
+	// print header
+	const char *file_name_sep = strrchr(file_name, '/');
+	if (!file_name_sep) {
+		file_name_sep = file_name;
+	} else {
+		file_name_sep += 1; // skip '/'
+	}
+	const char *file_name_dot = strchr(file_name_sep, '.');
+	if (!file_name_dot) {
+		file_name_dot = strchr(file_name_sep, '\0');
+	}
+
+	printf(">%.*s\n", (int)(file_name_dot - file_name_sep), file_name_sep);
+
+	// concat sequences
+	pfasta_seq ps;
+	while ((l = pfasta_read(&pf, &ps)) == 0) {
+		const char *seq = ps.seq;
+		// print sequence only
+		for (ssize_t j; *seq; seq += j) {
+			j = printf("%.*s\n", line_length, seq) - 1;
+			if (j < 0) {
+				err(errno, "error printing");
+			}
+		}
+
+		pfasta_seq_free(&ps);
+	}
+
+	if (l < 0) {
+		errx(1, "%s: %s", file_name, pfasta_strerror(&pf));
+	}
+
+	pfasta_free(&pf);
+	close(file_descriptor);
 }
 
-int main(int argc, const char *argv[]) {
+void usage(int exit_code) {
+	static const char str[] = {
+	    "Usage: concat [OPTIONS...] [FILE...]\n"
+	    "Concat multiple Fasta files into one sequence.\n"
+	    "When FILE is '-' read from standard input.\n\n"
+	    "Options:\n"
+	    "  -h         Display help and exit\n"
+	    "  -L num     Set the maximum line length (0 to disable)\n" //
+	};
 
-	if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'h') {
-		fprintf(stderr, "Usage: %s [FASTA...]\n", argv[0]);
-		return 1;
-	}
-
-	argv += 1;
-
-	int firsttime = 1;
-	int exit_code = EXIT_SUCCESS;
-
-	for (;; firsttime = 0) {
-		int file_descriptor;
-		const char *file_name;
-		if (!*argv) {
-			if (!firsttime) break;
-
-			file_descriptor = STDIN_FILENO;
-			file_name = "stdin";
-		} else {
-			file_name = *argv++;
-			file_descriptor = open(file_name, O_RDONLY);
-			if (file_descriptor < 0) err(1, "%s", file_name);
-		}
-
-		int l;
-		pfasta_file pf;
-		if ((l = pfasta_parse(&pf, file_descriptor)) != 0) {
-			warnx("%s: %s", file_name, pfasta_strerror(&pf));
-			exit_code = EXIT_FAILURE;
-			goto fail;
-		}
-
-		// print header
-		const char *file_name_sep = strrchr(file_name, '/');
-		if (!file_name_sep) {
-			file_name_sep = file_name;
-		} else {
-			file_name_sep += 1; // skip /
-		}
-		const char *file_name_dot = strchr(file_name_sep, '.');
-		if (!file_name_dot) {
-			file_name_dot = strchr(file_name_sep, '\0');
-		}
-
-		printf(">%.*s\n", (int)(file_name_dot - file_name_sep), file_name_sep);
-
-		// concat sequences
-		pfasta_seq ps;
-		while ((l = pfasta_read(&pf, &ps)) == 0) {
-			print_seq(&ps);
-			pfasta_seq_free(&ps);
-		}
-
-		if (l < 0) {
-			warnx("%s: %s", file_name, pfasta_strerror(&pf));
-			exit_code = EXIT_FAILURE;
-			pfasta_seq_free(&ps);
-		}
-
-	fail:
-		pfasta_free(&pf);
-		close(file_descriptor);
-	}
-
-	return exit_code;
+	fprintf(exit_code == EXIT_SUCCESS ? stdout : stderr, str);
+	exit(exit_code);
 }
