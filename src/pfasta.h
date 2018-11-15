ec023c5e68927a4c9d44510dbd4a5c198a5b5be6
Fabian Klötzl <fabian@kloetzl.info>
2018-11-15 11:33:15 +0100

    use pfasta14 as new implementation

--- src/pfasta.h
+++ src/pfasta.h
@@ -1,31 +1,39 @@
 #ifndef PFASTA_H
 #define PFASTA_H
 
+#include <threads.h>
+
 /** The following is the maximum length of an error string. It has to be
  * carefully chosen, so that all calls to PF_FAIL_STR succeed. For instance,
  * the line number can account for up to 20 characters.
  */
 #define PF_ERROR_STRING_LENGTH 100
 
-typedef struct pfasta_file {
-	char *buffer, *readptr, *fillptr;
-	char *errstr;
-	int errno__;
-	int fd;
-	size_t line;
-	char errstr_buf[PF_ERROR_STRING_LENGTH];
-	char unexpected_char;
-} pfasta_file;
+struct pfasta_record {
+	char *name, *comment, *sequence;
+	size_t name_length, comment_length, sequence_length;
+};
 
-typedef struct pfasta_seq {
-	char *name, *comment, *seq;
-} pfasta_seq;
+struct pfasta_parser {
+	int file_descriptor;
+	int done;
+	char *buffer;
+	char *read_ptr, *fill_ptr;
+	size_t line_number;
+	const char *errstr;
+};
 
-int pfasta_parse(pfasta_file *, int file_descriptor);
-void pfasta_free(pfasta_file *);
-void pfasta_seq_free(pfasta_seq *);
-int pfasta_read(pfasta_file *, pfasta_seq *);
+typedef struct dynstr {
+	char *str;
+	size_t capacity, count;
+} dynstr;
 
-const char *pfasta_strerror(const pfasta_file *);
+thread_local char errstr_buffer[PF_ERROR_STRING_LENGTH];
+
+void pfasta_record_free(struct pfasta_record *pr);
+void pfasta_free(struct pfasta_parser *pp);
+struct pfasta_parser pfasta_init(int file_descriptor);
+struct pfasta_record pfasta_read(struct pfasta_parser *pp);
+
 
 #endif /* PFASTA_H */
